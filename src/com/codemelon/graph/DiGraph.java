c22e7120242f950d5eb89776998b21ba098f9092
Marshall Farrier <info@marshallfarrier.com>
2012-11-24 13:16:16 -0800

    Removed vertex labels from DiGraph (and Graph)

--- src/com/codemelon/graph/DiGraph.java
+++ src/com/codemelon/graph/DiGraph.java
@@ -7,6 +7,10 @@
  */
 package com.codemelon.graph;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.codemelon.graph.vertex.Vertex;
@@ -16,40 +20,48 @@ import com.codemelon.graph.vertex.Vertex;
  * @version 11-23-2012
  */
 public class DiGraph {
-	private ConcurrentHashMap<Integer, Vertex> vertices;
+	private Set<Vertex> vertices;
 	private long edges;
 	
 	public DiGraph() {
-		vertices = new ConcurrentHashMap<Integer, Vertex>();
+		this(0);
+	}
+	public DiGraph(int initialCapacity) {
+		vertices = Collections.synchronizedSet(new HashSet<Vertex>(initialCapacity));
 		edges = 0L;
 	}
-	public DiGraph(int initialVertices) {
-		vertices = new ConcurrentHashMap<Integer, Vertex>(initialVertices);
-		for (int i = 0; i < initialVertices; i++) {
-			vertices.put(i, new Vertex());
+	/**
+	 * Initializes the Graph to contain the vertices in the collection
+	 * and clears the adjacency set of all vertices in the collection.
+	 * @param initialVertices collection of vertices to include in the Graph
+	 */
+	public DiGraph(Collection<Vertex> initialVertices) {
+		vertices = Collections.synchronizedSet(new HashSet<Vertex>(initialVertices));
+		edges = 0L;
+		for (Vertex v : initialVertices) {
+			v.clearAdjacencies();
 		}
 	}
 	/**
-	 * Inserts v if there is no vertex associated with the given label.
-	 * If there is already a vertex with this label, the method
-	 * does not insert the new vertex but returns the vertex to which
-	 * the label maps prior to the call.
-	 * @param label
-	 * @param v
-	 * @return
+	 * Inserts v if it is not already present.
+	 * @param v vertex to be inserted
+	 * @return true if the graph did not already contained the specified vertex
 	 */
-	public Vertex insert(int label, Vertex v) {
-		return vertices.putIfAbsent(label, v);
+	public boolean insert(Vertex v) {
+		return vertices.add(v);
 	}
-	/**
+ 	/**
 	 * Inserts edge if not already present. Returns false if the edge
 	 * was already present in the graph.
-	 * @param from label of the source vertex
-	 * @param to label of the target vertex
+	 * @param from source vertex
+	 * @param to target vertex
 	 * @return true if the edge was not previously present in the graph
 	 */
-	public boolean insertEdge(int from, int to) {
-		if (vertices.get(from).addAdjacency(vertices.get(to))) {
+	public boolean insertEdge(Vertex from, Vertex to) {
+		if (!vertices.contains(from) || !vertices.contains(to)) {
+			throw new IllegalArgumentException("Cannot add edge for vertex not present in graph!");
+		}
+		if (from.addAdjacency(to)) {
 			edges++;
 			return true;
 		}
@@ -58,16 +70,26 @@ public class DiGraph {
 	/**
 	 * Removes the specified edge if it is present. If the edge
 	 * is not found, returns false and does nothing.
-	 * @param from label of the edge's source vertex
-	 * @param to label of the edge's target vertex
+	 * @param from edge's source vertex
+	 * @param to edge's target vertex
 	 * @return true if the edge was found
 	 */
-	public boolean removeEdge(int from, int to) {
-		if (vertices.get(from).removeAdjacency(vertices.get(to))) {
+	public boolean removeEdge(Vertex from, Vertex to) {
+		if (!vertices.contains(from) || !vertices.contains(to)) {
+			throw new IllegalArgumentException("Cannot remove edge for vertex not present in graph!");
+		}
+		if (from.removeAdjacency(to)) {
 			edges--;
 			return true;
 		}
 		return false;
+	}
+	/**
+	 * Number of vertices in the graph
+	 * @return number of vertices in the graph
+	 */
+	public int vertices() {
+		return vertices.size();
 	}
 	/**
 	 * Number of edges in the graph
--- src/com/codemelon/graph/Graph.java
+++ src/com/codemelon/graph/Graph.java
@@ -3,23 +3,28 @@
  */
 package com.codemelon.graph;
 
+import java.util.Collection;
+
+import com.codemelon.graph.vertex.Vertex;
+
 /**
  * @author Marshall Farrier
  * @version 11-24-2012
  */
 public class Graph extends DiGraph{
 	public Graph() { super(); }
-	public Graph(int initialVertices) { super(initialVertices); }
+	public Graph(int initialCapacity) { super(initialCapacity); }
+	public Graph(Collection<Vertex> initialVertices) { super(initialVertices); }
 	@Override
-	public boolean insertEdge(int from, int to) {
+	public boolean insertEdge(Vertex from, Vertex to) {
 		if (!insertEdge(from, to)) { return false; }
 		return insertEdge(to, from);
 	}
 	@Override
-	public boolean removeEdge(int from, int to) {
+	public boolean removeEdge(Vertex from, Vertex to) {
 		if (!removeEdge(from, to)) { return false; }
 		return removeEdge(to, from);
 	}
 	@Override
-	public long edges() { return super.edges() / 2; }
+	public long edges() { return super.edges() / 2L; }
 }
