d3f7f049cc191c8b72988b36e8c138191d363e1e
Marshall Farrier <info@marshallfarrier.com>
2012-11-29 01:12:58 -0800

    Comments for Javadoc

--- src/com/codemelon/graph/common/Color.java
+++ src/com/codemelon/graph/common/Color.java
@@ -4,9 +4,22 @@
 package com.codemelon.graph.common;
 
 /**
+ * For coloring vertices or edges. Used in many search algorithms to track
+ * which vertices have been visited.
  * @author Marshall Farrier
  * @version 11/24/2012
  */
 public enum Color {
-	WHITE, GRAY, BLACK;
+	/**
+	 * For coloring vertices or edges white
+	 */
+	WHITE, 
+	/**
+	 * For coloring vertices or edges gray
+	 */
+	GRAY,
+	/**
+	 * For coloring vertices or edges black
+	 */
+	BLACK;
 }
