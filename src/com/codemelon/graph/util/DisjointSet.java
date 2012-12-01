cdbaec569e239d54ae83839d135ab4550da5d289
Marshall Farrier <info@marshallfarrier.com>
2012-12-01 11:13:02 -0800

    Implemented disjoint set

--- src/com/codemelon/graph/util/DisjointSet.java
+++ src/com/codemelon/graph/util/DisjointSet.java
@@ -1,54 +1,96 @@
 package com.codemelon.graph.util;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 
 /**
  * Implementation of disjoint set following 
  * <a href="http://mitpress.mit.edu/algorithms/">CLRS</a>, pp. 561ff.
- * Disjoint sets are needed for Kruskal's algorithm for finding a minimum
- * spanning tree.
+ * Disjoint sets are needed for Kruskal's minimum spanning tree algorithm.
  * @author Marshall Farrier
  * @version Dec 1, 2012
  */
 public class DisjointSet<T> {
-	private class LinkedItem {
-		T next;
-		T head;
-		T tail;
-		int size;
-		LinkedItem(T item) {
+	/*
+	 * A node contains a pointer to a set and to a next node
+	 */
+	private class Node {
+		T item;
+		Node next;
+		SetData setData;
+		Node(T item) {
+			this.item = item;
 			next = null;
-			head = item;
-			tail = item;
+			setData = new SetData(this);
+		}
+	}
+	/*
+	 * 1 such object for each set. As union operations occur,
+	 * 1 set is abandoned for garbage collection while the other
+	 * set is updated to reflect the union.
+	 */
+	private class SetData {
+		Node head;
+		Node tail;
+		int size;
+		SetData(Node node) {
+			head = node;
+			tail = node;
 			size = 1;
 		}
 	}
-	private HashMap<T, LinkedItem> linkedItems;
+	private HashMap<T, Node> nodes;
+
 	/**
-	 * This performs the make-set operation of CLRS on all items
+	 * This performs the make-set operation described in 
+	 * <a href="http://mitpress.mit.edu/algorithms/">CLRS</a> on all items
 	 * in the collection
 	 * @param items
 	 */
 	public DisjointSet(Collection<T> items) {
-		linkedItems = new HashMap<T, LinkedItem>(items.size());
+		nodes = new HashMap<T, Node>(items.size());
 		for (T item : items) {
-			linkedItems.put(item, new LinkedItem(item));
+			nodes.put(item, new Node(item));
 		}
 	}
 	
 	public T findSet(T item) {
-		return linkedItems.get(item).head;
+		return nodes.get(item).setData.head.item;
 	}
 	
 	/**
-	 * CLRS, p. 562
-	 * @param x
-	 * @param y
+	 * Perform a union on the sets to which x and y belong.
+	 * After this method is called, x and y will belong to the same
+	 * disjoint set.
+	 * <a href="http://mitpress.mit.edu/algorithms/">CLRS</a>, p. 562
+	 * @param x member of a disjoint set (unknown whether or not the same set as y)
+	 * @param y member of a disjoint set (unknown whether or not the same set as x)
+	 * @return true iff the sets to which x and y belong were distinct
+	 * prior to the call
 	 */
-	public void union(T x, T y) {
-		
+	public boolean union(T x, T y) {
+		Node bigSetHead = nodes.get(x).setData.head;
+		Node smallSetHead = nodes.get(x).setData.head;
+		Node tmp;
+		if (bigSetHead == smallSetHead) { return false; }
+		// swap if necessary so that bigSetHead really points to the larger set
+		if (bigSetHead.setData.size < smallSetHead.setData.size) {
+			tmp = bigSetHead;
+			bigSetHead = smallSetHead;
+			smallSetHead = tmp;
+		}
+		// update the size of the new set
+		bigSetHead.setData.size += smallSetHead.setData.size;
+		// update the next pointer for tail of big set
+		bigSetHead.setData.tail.next = smallSetHead;
+		// update the tail pointer for the big set
+		bigSetHead.setData.tail = smallSetHead.setData.tail;
+		// point all nodes in the small set to the big set setData
+		tmp = smallSetHead;
+		while (tmp != null) {
+			tmp.setData = bigSetHead.setData;
+			tmp = tmp.next;
+		}
+		return true;
 	}
 }
