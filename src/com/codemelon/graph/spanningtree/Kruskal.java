4224a9a35e76c20d5ac505539ad823975e54b645
Marshall Farrier <info@marshallfarrier.com>
2012-12-01 14:39:24 -0800

    Spanningtree package for Kruskal's and Prim's algorithms.

---
+++ src/com/codemelon/graph/spanningtree/Kruskal.java
@@ -0,0 +1,16 @@
+package com.codemelon.graph.spanningtree;
+
+import com.codemelon.graph.Graph;
+
+/**
+ * Implementation of Kruskal's algorithm for growing a minimum spanning
+ * tree. Follows
+ * <a href="http://mitpress.mit.edu/algorithms/">CLRS</a>, pp. 631ff.
+ * @author Marshall Farrier
+ * @version Dec 1, 2012
+ *
+ */
+public class Kruskal {
+	private Graph graph;
+
+}
---
+++ src/com/codemelon/graph/spanningtree/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ * Algorithms for finding a minimum spanning tree: Kruskal's
+ * algorithm and Prim's algorithm
+ * 
+ * @author Marshall Farrier
+ * @version Dec 1, 2012
+ *
+ */
+package com.codemelon.graph.spanningtree;
