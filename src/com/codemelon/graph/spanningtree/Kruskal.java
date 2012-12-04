aadd02b8b5f94acbabd683a233c385f78ffce789
Marshall Farrier <info@marshallfarrier.com>
2012-12-03 22:21:31 -0800

    Unit tests for EdgeResetter, expansion of Kruskal class

--- src/com/codemelon/graph/spanningtree/Kruskal.java
+++ src/com/codemelon/graph/spanningtree/Kruskal.java
@@ -2,6 +2,9 @@ package com.codemelon.graph.spanningtree;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import com.codemelon.graph.Graph;
 import com.codemelon.graph.common.Color;
@@ -24,6 +27,8 @@ public class Kruskal {
 	private Graph graph;
 	private WeightedEdge[] edges;
 	private DisjointSet<Vertex> vertexDisjointSet;
+	private HashMap<Vertex, Vertex> vertexMap;
+	private Graph spanningTree;
 	
 	private static final WeightedEdge[] EMPTY_EDGE_ARRAY = new WeightedEdge[0];
 	private static final Comparator<WeightedEdge> COMPARATOR = new Comparator<WeightedEdge>() {
@@ -37,15 +42,16 @@ public class Kruskal {
 	
 	public Kruskal(Graph graph) {
 		this.graph = graph;
-		edges = graph.getWeightedEdges().toArray(EMPTY_EDGE_ARRAY);
-		Arrays.sort(edges, COMPARATOR);
-		vertexDisjointSet = new DisjointSet<Vertex>(graph.getVertices());
+		edges = null;
+		vertexDisjointSet = null;
+		vertexMap = null;
+		spanningTree = null;
 	}
 	/**
 	 * Set edges in minimum spanning tree to BLACK, all other edges are WHITE
 	 */
 	public void markEdges() {
-		new EdgeResetter(graph).resetColors();
+		initializeForMarking();
 		for (WeightedEdge edge : edges) {
 			if (vertexDisjointSet.findSet(edge.from()) 
 					!= vertexDisjointSet.findSet(edge.to())) {
@@ -53,5 +59,60 @@ public class Kruskal {
 				vertexDisjointSet.union(edge.from(), edge.to());				
 			}
 		}		
+	}
+	/**
+	 * Creates and returns a minimum spanning tree. Running this method does not modify
+	 * the original graph. Corresponding vertices can be determined by running getVertexMap().
+	 * The 2 graphs will have the same weight epsilon for
+	 * determining the accuracy used for weight equality, and the weights of edges between
+	 * corresponding vertices will be the same. With the exception of edge weights and weight epsilon,
+	 * however, all vertex and edge data in the spanning tree will be set to the default value,
+	 * regardless of the corresponding value in the original graph.
+	 * @return a spanning tree of the original graph.
+	 */
+	public Graph generateTree() {
+		initializeForGenerateTree();
+		for (WeightedEdge edge : edges) {
+			if (vertexDisjointSet.findSet(edge.from()) 
+					!= vertexDisjointSet.findSet(edge.to())) {
+				spanningTree.addEdge(vertexMap.get(edge.from()), vertexMap.get(edge.to()));
+				vertexMap.get(edge.from()).setEdgeWeight(vertexMap.get(edge.to()), edge.weight());
+				vertexDisjointSet.union(edge.from(), edge.to());				
+			}
+		}		
+		return spanningTree;
+	}
+	/**
+	 * Returns a map showing which vertex in the spanning tree corresponds to a given
+	 * vertex in the original graph. Edges between corresponding vertices in the 2 graphs
+	 * will have the same weight. 
+	 * @return
+	 */
+	public Map<Vertex, Vertex> getVertexMap() {
+		if (vertexMap == null) {
+			throw new IllegalStateException("Map must first be created by running generateTree()!");
+		}
+		return vertexMap;
+	}
+	private void initializeForGenerateTree() {
+		vertexMap = new HashMap<Vertex, Vertex>(graph.vertexCount());
+		spanningTree = new Graph(graph.vertexCount());
+		spanningTree.setWeightEpsilon(graph.getWeightEpsilon());
+		Iterator<Vertex> it = graph.vertexIterator();
+		Vertex v;
+		while (it.hasNext()) {
+			v = it.next();
+			vertexMap.put(v, new Vertex());
+			spanningTree.addVertex(vertexMap.get(v));
+		}
+		vertexDisjointSet = new DisjointSet<Vertex>(graph.getVertices());
+		edges = graph.getWeightedEdges().toArray(EMPTY_EDGE_ARRAY);
+		Arrays.sort(edges, COMPARATOR);		
+	}
+	private void initializeForMarking() {
+		new EdgeResetter(graph).resetColors();
+		vertexDisjointSet = new DisjointSet<Vertex>(graph.getVertices());
+		edges = graph.getWeightedEdges().toArray(EMPTY_EDGE_ARRAY);
+		Arrays.sort(edges, COMPARATOR);
 	}
 }
---
+++ tst/com/codemelon/graph/util/EdgeResetterTest.java
@@ -0,0 +1,165 @@
+package com.codemelon.graph.util;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.codemelon.graph.DiGraph;
+import com.codemelon.graph.common.Color;
+import com.codemelon.graph.common.EdgeType;
+import com.codemelon.graph.edge.EdgeConstants;
+import com.codemelon.graph.vertex.Vertex;
+
+/**
+ * @author Marshall Farrier
+ * @version Dec 3, 2012
+ */
+public class EdgeResetterTest {
+	private static final int VERTICES_IN_TEST_GRAPH = 100;
+	private static final int ODD_VERTEX_TAIL_INDEX = 35;
+	private static final int ODD_VERTEX_HEAD_INDEX = 79;
+	private static final Color OFF_COLOR = Color.GRAY;
+	private static final Color NEW_COLOR = Color.BLACK;
+	private static final EdgeType OFF_TYPE = EdgeType.BACK;
+	private static final EdgeType NEW_TYPE = EdgeType.TREE;
+	private static final double OFF_WEIGHT = 2.71828;
+	private static final double NEW_WEIGHT = 3.14159;
+	private DiGraph graph;
+	private ArrayList<Vertex> vertices;
+	private EdgeResetter edgeResetter;
+
+	/**
+	 * 
+	 */
+	@Before
+	public void setUp() {
+		vertices = new ArrayList<Vertex>(VERTICES_IN_TEST_GRAPH);
+		for (int i = 0; i < VERTICES_IN_TEST_GRAPH; i++) {
+			vertices.add(new Vertex(i));
+		}
+		graph = new DiGraph(vertices);// connect all vertices
+		for (int i = 0; i < VERTICES_IN_TEST_GRAPH; i++) {
+			for (int j = 0; j < VERTICES_IN_TEST_GRAPH; j++) {
+				graph.addEdge(vertices.get(i), vertices.get(j));
+			}
+		}
+		edgeResetter = new EdgeResetter(graph);
+	}
+
+	/**
+	 * 
+	 */
+	@After
+	public void tearDown() {
+		graph = null;
+		vertices = null;
+		edgeResetter = null;
+	}
+
+	/**
+	 * Test method for {@link com.codemelon.graph.util.EdgeResetter#resetColors(com.codemelon.graph.common.Color)}.
+	 */
+	@Test
+	public void testResetColorsColor() {
+		// set 1 edge to a different color
+		graph.setEdgeColor(vertices.get(ODD_VERTEX_TAIL_INDEX), vertices.get(ODD_VERTEX_HEAD_INDEX),
+				OFF_COLOR);
+		edgeResetter.resetColors(NEW_COLOR);
+		for (int i = 0; i < VERTICES_IN_TEST_GRAPH; i++) {
+			for (int j = 0; j < VERTICES_IN_TEST_GRAPH; j++) {
+				assertEquals("Correct color", NEW_COLOR, graph.getEdgeColor(vertices.get(i), 
+						vertices.get(j)));
+			}
+		}
+	}
+
+	/**
+	 * Test method for {@link com.codemelon.graph.util.EdgeResetter#resetColors()}.
+	 */
+	@Test
+	public void testResetColors() {
+		// set 1 edge to a different color
+		graph.setEdgeColor(vertices.get(ODD_VERTEX_TAIL_INDEX), vertices.get(ODD_VERTEX_HEAD_INDEX),
+				OFF_COLOR);
+		edgeResetter.resetColors();
+		for (int i = 0; i < VERTICES_IN_TEST_GRAPH; i++) {
+			for (int j = 0; j < VERTICES_IN_TEST_GRAPH; j++) {
+				assertEquals("Correct default color", EdgeConstants.DEFAULT_COLOR, graph.getEdgeColor(vertices.get(i), 
+						vertices.get(j)));
+			}
+		}
+	}
+
+	/**
+	 * Test method for {@link com.codemelon.graph.util.EdgeResetter#resetEdgeTypes(com.codemelon.graph.common.EdgeType)}.
+	 */
+	@Test
+	public void testResetEdgeTypesEdgeType() {
+		// set 1 edge to a different type
+		graph.setEdgeType(vertices.get(ODD_VERTEX_TAIL_INDEX), vertices.get(ODD_VERTEX_HEAD_INDEX),
+				OFF_TYPE);
+		edgeResetter.resetEdgeTypes(NEW_TYPE);
+		for (int i = 0; i < VERTICES_IN_TEST_GRAPH; i++) {
+			for (int j = 0; j < VERTICES_IN_TEST_GRAPH; j++) {
+				assertEquals("Correct type", NEW_TYPE, graph.getEdgeType(vertices.get(i), 
+						vertices.get(j)));
+			}
+		}
+	}
+
+	/**
+	 * Test method for {@link com.codemelon.graph.util.EdgeResetter#resetEdgeTypes()}.
+	 */
+	@Test
+	public void testResetEdgeTypes() {
+		// set 1 edge to a different type
+		graph.setEdgeType(vertices.get(ODD_VERTEX_TAIL_INDEX), vertices.get(ODD_VERTEX_HEAD_INDEX),
+				OFF_TYPE);
+		edgeResetter.resetEdgeTypes();
+		for (int i = 0; i < VERTICES_IN_TEST_GRAPH; i++) {
+			for (int j = 0; j < VERTICES_IN_TEST_GRAPH; j++) {
+				assertEquals("Correct default type", EdgeConstants.DEFAULT_EDGE_TYPE, 
+						graph.getEdgeType(vertices.get(i), vertices.get(j)));
+			}
+		}
+	}
+
+	/**
+	 * Test method for {@link com.codemelon.graph.util.EdgeResetter#resetWeights(double)}.
+	 */
+	@Test
+	public void testResetWeightsDouble() {
+		// set 1 edge to a different type
+		graph.setEdgeWeight(vertices.get(ODD_VERTEX_TAIL_INDEX), vertices.get(ODD_VERTEX_HEAD_INDEX),
+				OFF_WEIGHT);
+		edgeResetter.resetWeights(NEW_WEIGHT);
+		for (int i = 0; i < VERTICES_IN_TEST_GRAPH; i++) {
+			for (int j = 0; j < VERTICES_IN_TEST_GRAPH; j++) {
+				assertTrue("Correct weight", graph.areEqualWeights(NEW_WEIGHT, graph.getEdgeWeight(vertices.get(i), 
+						vertices.get(j))));
+			}
+		}
+	}
+
+	/**
+	 * Test method for {@link com.codemelon.graph.util.EdgeResetter#resetWeights()}.
+	 */
+	@Test
+	public void testResetWeights() {
+		// set 1 edge to a different type
+		graph.setEdgeWeight(vertices.get(ODD_VERTEX_TAIL_INDEX), vertices.get(ODD_VERTEX_HEAD_INDEX),
+				OFF_WEIGHT);
+		edgeResetter.resetWeights();
+		for (int i = 0; i < VERTICES_IN_TEST_GRAPH; i++) {
+			for (int j = 0; j < VERTICES_IN_TEST_GRAPH; j++) {
+				assertTrue("Correct default weight", 
+						graph.areEqualWeights(EdgeConstants.DEFAULT_WEIGHT, graph.getEdgeWeight(vertices.get(i), 
+						vertices.get(j))));
+			}
+		}
+	}
+}
